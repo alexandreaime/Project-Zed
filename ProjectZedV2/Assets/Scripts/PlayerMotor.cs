6b702a48ceafb56fac9fa5a5080ea2dfaab5b7f9
alexandreaime <54902943+alexandreaime@users.noreply.github.com>
2020-03-10 14:30:55 +0100

    EP4 fini du tuto (synchronisation joueur)

---
+++ ProjectZedV2/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+    
+    private Rigidbody rb;
+    
+    /*public float gravity = -39.24f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;*/
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation; 
+    }
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation; 
+    }
+    
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+
+        /*isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }*/
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        cam.transform.Rotate(-cameraRotation);
+    }
+}
