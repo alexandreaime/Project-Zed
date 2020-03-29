17c20df9e4c1f2dbdd3ad0c3071f61b3ee48caa1
alexandreaime <54902943+alexandreaime@users.noreply.github.com>
2020-03-29 14:48:10 +0200

    Configuation Multi (Host Game) + Début Join Game

---
+++ ProjectZedV2/Assets/Scripts/Multi/JoinGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour
+{
+    List<GameObject> roomList = new List<GameObject>();
+    [SerializeField] private Text status;
+    [SerializeField] private GameObject roomListItemPrefab;
+    [SerializeField] private Transform roomListParent;
+    
+    private NetworkManager networkManager;
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Refreshing...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        status.text = "";
+        if (matchList == null)
+        {
+            status.text = "Couldn't get match list.";
+            return;
+        }
+
+        ClearRoomList();
+
+        foreach (MatchInfoSnapshot match in matchList)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+        }
+    }
+
+    private void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+    }
+}
