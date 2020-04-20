4cca0040abd423278741a7258372121fd12e9f9b
philippegdl <55808249+philippegdl@users.noreply.github.com>
2020-04-20 17:44:59 +0200

    look at me

---
+++ ProjectZedV2/Assets/Scripts/Enemy/LookAtPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    //Player
+    public Transform target;
+    private Player player;
+
+    float speed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject nearestPlayer = null;
+
+        float shortestDistance = Mathf.Infinity;
+
+        foreach (GameObject player in players)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestPlayer = player;
+            }
+
+            target = nearestPlayer.transform;
+            this.player = target.GetComponent<Player>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+    }
+}
