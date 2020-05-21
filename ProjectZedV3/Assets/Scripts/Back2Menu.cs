aba2dfbb93d76f966e12e45ae76b67bed5730984
alexandreaime <54902943+alexandreaime@users.noreply.github.com>
2020-05-21 23:36:48 +0200

    Ajout bouton Back2Menu au lobby + loginmenu

---
+++ ProjectZedV3/Assets/Scripts/Back2Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back2Menu : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
