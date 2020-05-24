11090c5009db3ba5205311edaf15dc137751176d
alexandreaime <54902943+alexandreaime@users.noreply.github.com>
2020-05-24 16:11:37 +0200

    Tir une seule grenade

--- ProjectZedV3/Assets/Scripts/Arme/EjectScript.cs
+++ ProjectZedV3/Assets/Scripts/Arme/EjectScript.cs
@@ -8,14 +8,22 @@ public class EjectScript : MonoBehaviour
     [SerializeField] GameObject grenade;
     [SerializeField] int force = 800;
 
+    private bool throwing;    
+
 
     void Update()
     {
+        ThrowGrenade();
+    }
 
-        if (Input.GetKey(KeyCode.G))
+    public void ThrowGrenade()
+    {
+        if (Input.GetKey(KeyCode.G) && !throwing)
         {
             GameObject Go = Instantiate(grenade, transform.position, Quaternion.identity);
             Go.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * force);
+            
+            throwing = true;
         }
     }
 }
