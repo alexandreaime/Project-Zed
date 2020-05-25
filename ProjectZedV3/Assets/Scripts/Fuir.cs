d76f03132886916097b3dc3636770a653db0cc55
alexandreaime <54902943+alexandreaime@users.noreply.github.com>
2020-05-26 00:16:40 +0200

    New button lobby + qlq tite modif

---
+++ ProjectZedV3/Assets/Scripts/Fuir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fuir : MonoBehaviour
+{
+    public void QuittheGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
